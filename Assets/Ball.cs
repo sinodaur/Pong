4ed32c9d34e51424d647a3181ce5ceda77db94e1
sinodaur <cperric@gmail.com>
2016-01-01 18:26:58 -0500

    Start of project
    Player.cs to control a pong stick
    ball.cs to give the ball momentum
    added rigidbody to ball
    next will try to keep score

---
+++ Assets/Ball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public float thrust = 500;
+    public Rigidbody rb;
+    Vector3 left = new Vector3(-1.0f, .01f, 0f);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(rb.transform.forward);
+        rb.AddForce(left * thrust);
+    }
+
+   
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+
+    void Start ()
+    {
+	
+	}
+	
+	
+	void Update ()
+    {
+        
+        float translation = Input.GetAxis("Vertical") * speed;
+        translation *= Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        transform.position = new Vector3(transform.position.x, translation + transform.position.y, 0);
+        rotation *= Time.deltaTime;
+        transform.Rotate(0, 0, rotation);
+
+    }
+}
